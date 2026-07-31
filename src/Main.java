651c6f09280b097b04243fb799b138eed9368253
YODA_Ghaniyou <yodaghaniyou@gmail.com>
2026-07-31 21:02:29 +0000

    Ajout de la classe Emprunt reliant Livre et Membre

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         Livre livre1 = new Livre("Le Petit Prince", "Antoine de Saint-Exupéry", "978-2070408504");
+        Membre membre1 = new Membre("Jean Dupont", "M001");
+
+        Emprunt emprunt1 = new Emprunt(livre1, membre1);
+        System.out.println(emprunt1);
         System.out.println(livre1);
 
-        Membre membre1 = new Membre("Jean Dupont", "M001");
-        System.out.println(membre1);
+        emprunt1.retournerLivre();
+        System.out.println("Après retour : " + livre1);
     }
 }
